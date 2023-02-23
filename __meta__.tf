8157e17ee33308b67e2a6bb4993edd23199e4708
Pavel Pichrt <pav@pav.dev>
2023-02-23 15:47:22 +0000

    Added the missing provider block

--- __meta__.tf
+++ __meta__.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 4.20.0"
+    }
+  }
+}
+
+# ===========================
+
 variable "name" {}
 
 variable "sse_algorithm" {
